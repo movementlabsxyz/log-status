1d8189608bd7841f4d05a1593a9d2cd5971d5642
Liam Monninger <l.mak.monninger@gmail.com>
2024-01-23 20:18:55 -0800

    feat: initialize db task.

---
+++ tasks/initialize-db/init-db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS LatestHealthChecks (
+    PRIMARY KEY (health_check, "group"),
+    health_check VARCHAR(255),
+    "group" VARCHAR(255),
+    status BOOLEAN,
+    reason TEXT
+);
+
+-- Add health checks
+INSERT INTO LatestHealthChecks (health_check, "group", status, reason) VALUES 
+('log-table-creation', 'LogStatus', true, 'Pipeline was able to initialize LogStatus table.'),
